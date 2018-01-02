661db87328437fcbaed81c66ef185e1e024bdc3d
Darren Neimke <darren.neimke@live.com>
2018-01-02 12:58:52 +1030

    Adds basic example showing subscriptions

--- scripts/from-event.ts
+++ scripts/from-event.ts
@@ -1,18 +1,47 @@
 import { fromEvent } from "rxjs/observable/fromEvent";
-import { map } from "rxjs/operators/map";
+import "rxjs/add/operator/map";
+import { Subscription } from "rxjs/Subscription";
 
 export class FromEvent {
-  constructor(buttonElementId: string) {
-    const btn = document.getElementById("myButton") as HTMLButtonElement;
-    const source = fromEvent(btn, "click");
-    const example = source.subscribe(e => console.log("Clicked: ", e));
+  isSubscribed = true;
+  subscription: Subscription;
+
+  constructor(buttonElementId: string, subscribeButtonId: string) {
+    const btn = document.getElementById(buttonElementId) as HTMLButtonElement;
+    this.subscribe(btn);
+
+    const subscribeButton = document.getElementById(
+      subscribeButtonId
+    ) as HTMLButtonElement;
+
+    fromEvent(subscribeButton, "click").subscribe(e => {
+      if (this.isSubscribed) {
+        this.unsubscribe();
+        subscribeButton.innerText = "Subscribe";
+      } else {
+        this.subscribe(btn);
+        subscribeButton.innerText = "Unsubscribe";
+      }
+      this.isSubscribed = !this.isSubscribed;
+    });
+  }
+
+  subscribe(btn: HTMLButtonElement) {
+    const source = fromEvent(btn, "click").map(
+      (event: Event) => `Event time: ${event.timeStamp}`
+    );
+    this.subscription = source.subscribe(e => console.info("Clicked: ", e));
+  }
+
+  unsubscribe() {
+    this.subscription.unsubscribe();
   }
 }
 
 export class FromEventPageRunner {
-  constructor(elementId: string) {
-    const runner = new FromEvent(elementId);
+  constructor(elementId: string, subscribeButtonId: string) {
+    const runner = new FromEvent(elementId, subscribeButtonId);
   }
 }
 
-let runner = new FromEventPageRunner("myButton");
+let runner = new FromEventPageRunner("myButton", "subscribeButton");
